c9d6271e425856b84a7d40bc946d684690781a8b
kb21-doofy <arg.kaws@gmail.com>
2025-01-25 19:00:24 +0900

    change str to float

--- src/back/get_screenshot.py
+++ src/back/get_screenshot.py
@@ -1,4 +1,4 @@
-def get_screenshot(screenshot: str):
+def get_screenshot(screenshot: float):
     screenshot_path = Users/a/"Desktop"/screenshot
     file_size = screenshot_path.stat().st_size
     
